8b1acc33e32639d9b4849634b136b52a639b2171
OlgaCheremisinova <cheremisinova8@gmail.com>
2021-02-02 15:00:39 +0300

    fix

--- day_1/binary_gap.go
+++ day_1/binary_gap.go
@@ -5,10 +5,10 @@ import (
 	"strconv"
 )
 
-func solution(n int) int {
+func Solution(N int) int {
 	count := 0
 	max := 0
-	binaryNumber := strconv.FormatInt(int64(n), 2)
+	binaryNumber := strconv.FormatInt(int64(N), 2)
 
 	for _, letter := range binaryNumber {
 		if string(letter) == "0" {
@@ -30,5 +30,5 @@ func main() {
 	var i int
 	fmt.Println("Введите число: ")
 	fmt.Scanf("%d", &i)
-	fmt.Println(solution(i))
+	fmt.Println(Solution(i))
 }
